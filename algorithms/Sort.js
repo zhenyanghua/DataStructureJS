b79f47debbab7c84c79285d0a17e17b7457cfe3b
Zhenyang Hua <zhenyang.hua@woolpert.com>
2017-01-31 22:32:32 -0500

    add selection sort

--- algorithms/Sort.js
+++ algorithms/Sort.js
@@ -15,16 +15,17 @@ ArrayList.prototype.toString = function() {
 	return this.array.join();
 }
 
-ArrayList.prototype.print = function() {
-	console.log(this.toString());
+ArrayList.prototype.print = function(msg) {
+	var prefix = msg ? "*" + msg + "* - " : "";
+	console.log(prefix + this.toString());
 }
 
 
-ArrayList.prototype.swap = function(array, i, j) {
-	var tmp = array[i];
-	array[i] = array[j];
-	array[j] = tmp;
-	console.log('after swap', array)
+ArrayList.prototype.swap = function(i, j) {
+	var tmp = this.array[i];
+	this.array[i] = this.array[j];
+	this.array[j] = tmp;
+	console.log('after swap', this.array)
 }
 
 /**
@@ -34,19 +35,69 @@ ArrayList.prototype.bubbleSort = function() {
 	for (var i = 0; i < this.array.length; i++) {
 		for (var j = 0; j < this.array.length - 1 - i; j++) {
 			if (this.array[j] > this.array[j+1])
-				this.swap(this.array, j, j+1);
+				this.swap(j, j+1);
 		}
 	}
 }
 
 /**
+ * Test Selection Sort
+ */
+ArrayList.prototype.selectionSort = function() {
+	var minInd;
+	for (var i = 0; i < this.array.length; i++) {
+		minInd = i;
+		for (var j = i; j < this.array.length; j++) {
+			if (this.array[j] < this.array[i]) minInd = j;
+		}
+		if (minInd !== i) this.swap(i, minInd)
+	}
+}
+
+/**
+ * Usage - Creating an unsorted list
+ */
+function createUnsortedList() {
+	var list = new ArrayList();
+	list.insert(5, 4, 3, 2, 1);
+	list.print('Unsorted');
+	printDash();
+	return list;
+}
+
+function printDash() {
+	console.log('-----------------------------');
+}
+
+function printTest(msg) {
+	printDash();
+	console.log(msg);
+	printDash();
+}
+
+function formatPrint(list) {
+	printDash();
+	list.print('Sorted');
+	printDash();
+}
+
+/**
  * Test Bubble Sort
  */
-var list = new ArrayList();
-list.insert(5, 4, 3, 2, 1);
-list.print()
+printTest('Using Bubble Sort.');
+var list = createUnsortedList();
 list.bubbleSort();
-list.print();
+formatPrint(list);
+
+/**
+ * Test Selection Sort
+ */
+printTest('Using Selection Sort.');
+list = createUnsortedList();
+list.selectionSort();
+formatPrint(list);
+
+
 
 
 
