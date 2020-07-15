46940ef64bc5156a9d0838a3c6a6f3eb6e5eb1a9
Zhenyang Hua <zhenyang.hua@gmail.com>
2020-07-14 22:07:05 -0400

    Fixed a bug on the array initialization with fill.

--- classics/TowerOfHanoiN.js
+++ classics/TowerOfHanoiN.js
@@ -2,44 +2,56 @@ import Stack from '../datastructures/Stack.js';
 
 let moved = 0;
 
-function move(beginStack, endStack, tempStacks, diskNumber) {
+const stackA = new Stack();
+for (let i = 1; i <= 4; i++) {
+  stackA.push(i);
+}
+const stackC = new Stack();
+const assistStacks = new Array(2).fill(0).map(() => new Stack());
+
+function move(beginStack, endStack, tempStacks, numberOfDisks) {
   // If there is only one disk left, move it to the end stack.
   // Else, move everything above it to the temp stack,
-    // where for each extra temp stack takes the last of the disk
+  //  - where for each extra temp stack takes the last of the disk
   // then move the last disk to the end stack,
-    // then for each extra temp stack, move each disk to the end stack.
+  //  - then for each extra temp stack, move each disk to the end stack.
   // then do the same to the rest of the temp stack.
-  moved++;
 
-  if (diskNumber === 1) {
-    console.log(beginStack, tempStacks, endStack);
+  if (numberOfDisks === 1) {
     endStack.push(beginStack.pop());
-    console.log('-----');
-  } else if (diskNumber > 1){
-    move(beginStack, tempStacks[tempStacks.length - 1], [endStack, ...tempStacks.slice(0, tempStacks.length - 1)], diskNumber - (tempStacks.length - 1) - 1);
-    for (let i = 0; i < tempStacks.length - 1; i++) {
+    console.log(++moved, stackA, assistStacks, stackC);
+  } else if (numberOfDisks > 1) {
+    move(
+      beginStack,
+      tempStacks[tempStacks.length - 1],
+      [...tempStacks.slice(0, tempStacks.length - 1), endStack],
+      numberOfDisks - tempStacks.length
+    );
+    const moveUntilDisk = tempStacks.length - 1;
+    for (let i = 0; i < moveUntilDisk && !beginStack.isEmpty(); i++) {
       tempStacks[i].push(beginStack.pop());
+      console.log(++moved, stackA, assistStacks, stackC);
     }
     move(beginStack, endStack, tempStacks, 1);
     for (let i = tempStacks.length - 2; i >= 0; i--) {
-      move(tempStacks[i], endStack, [beginStack, ...tempStacks.slice(0, i), ...tempStacks.slice(i + 1)], 1);
+      const disk = tempStacks[i].pop();
+      if (disk) {
+        endStack.push(disk);
+        console.log(++moved, stackA, assistStacks, stackC);
+      }
     }
-    move(tempStacks[tempStacks.length - 1], endStack, [...tempStacks.slice(0, tempStacks.length - 1), beginStack], diskNumber - (tempStacks.length - 1) - 1);
+
+    move(
+      tempStacks[tempStacks.length - 1],
+      endStack,
+      [beginStack, ...tempStacks.slice(0, tempStacks.length - 1)],
+      numberOfDisks - tempStacks.length
+    );
   }
 }
 
-function hanoiN () {
-  const stackA = new Stack();
-  for (let i = 1; i <= 4; i++) {
-    stackA.push(i);
-  }
-  const stackC = new Stack();
-  const assistStacks = Array(3).fill(new Stack());
+console.log(stackA, assistStacks, stackC);
+console.log('Start Moving');
+move(stackA, stackC, assistStacks, stackA.size());
 
-  move(stackA, stackC, assistStacks, stackA.size());
-
-  console.log(stackA, assistStacks, stackC);
-  console.log('Moved ', moved);
-}
-
-hanoiN();
+console.log('Moved ', moved);
